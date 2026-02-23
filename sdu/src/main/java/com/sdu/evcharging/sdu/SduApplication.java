0963b79458a009086f54d59644ccec1f953036b6
Ahmed-Deeq Abdi <113026984+ahmeddeeqabdi@users.noreply.github.com>
2026-02-23 10:20:52 +0100

    Initial setup

---
+++ sdu/src/main/java/com/sdu/evcharging/sdu/SduApplication.java
@@ -0,0 +1,13 @@
+package com.sdu.evcharging.sdu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SduApplication.class, args);
+	}
+
+}
---
+++ sdu/src/test/java/com/sdu/evcharging/sdu/SduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdu.evcharging.sdu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
